4897e03da547decfd13f565ba151acebb0f15da9
unknown <phamthanhthuy15012004@gmail.com>
2025-10-15 19:49:41 +0700

    fix loi

---
+++ QLCVan/file1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace QLCVan
+{
+    public partial class file1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
